6ed3b0e341b60915a52687ad19bc26c41c85cadd
jose-alviarez <jose>
2023-09-18 14:55:47 -0400

    agregado dos ejercicios

--- asset/js/practica-3.js
+++ asset/js/practica-3.js
@@ -1,4 +1,4 @@
-//?primera parte
+// //?primera parte
 // Función para mostrar el menu y solicitar la eleccion al usuario
 function mostrarMenu() {
     let eleccion = prompt("Bienvenido al restaurante. Elija una opcion del menu:\n1. Carne (viene acompañado con vino tinto )\n2. Pescado (viene acompañado con vino blanco)\n3. Verdura (viene acompañado con agua )");
@@ -15,6 +15,7 @@ function mostrarMenu() {
       mostrarMenu();
     }
   }
+
   
   // Función para ofrecer la bebida correspondiente de cada platillo
   function ofrecerBebida(platillo) {
@@ -39,9 +40,110 @@ function mostrarMenu() {
     }
   }
   
-  //resultado
+  // resultado
   mostrarMenu();  
-//?primer parte-end
+// //?primer parte-end
+
+
+
+//?segunda parte
+// pide al usuario el valor
+let basee = prompt("Ingresa la longitud de la base:");
+let alto = prompt("Ingresa la altura:");
+
+// verifica si los valores ingresados son numeros
+if (isNaN(basee) || isNaN(alto)) {
+  console.log("Alguno de los valores ingresados no es válido.");
+} else if (basee === "" || alto === "") {
+  console.log("No ingresaste ningún valor.");
+} else {
+  // revisa si la base y el alto son iguales
+  if (basee === alto) {
+    console.log("Es un cuadrado");
+  } else if (basee > alto) {
+    console.log("Es un rectángulo horizontal");
+  } else {
+    console.log("Es un rectángulo vertical");
+  }
+}
+//?segunda parte-end
+
+  //?tercera parte
+  // mensaje para que el usuario ingrese la nota
+  let nota = parseInt(prompt("Ingresa tu nota:"));
+
+  //proceso de las notas que seran validas
+  function convertirNota(nota) {
+  if (nota >= 0 && nota <= 4) {
+        return "Suspenso";
+    } else if (nota >= 5 && nota <= 6) {
+        return "Aprobado";
+    } else if (nota >= 7 && nota <= 8) {
+        return "Notable";
+    } else if (nota >= 9 && nota <= 10) {
+        return "Sobresaliente";
+    } else {
+      //mensaje que saldra si no es un numero valido o si ingresa palabras 
+        return "Nota invalida";
+    }
+}
+
+// vinculacion de las notas con los datos que ingreso el usuario
+let calificacion = convertirNota(nota);
+//respuesta del programa segun el dato ingresado  
+console.log("Tu calificación es: " + calificacion);
+//?tercera parte-end
+
+
+
+
+
+// ?quinta parte
+// solicitud de que el usuario ingrese un numero
+let numero = prompt("Ingresa un número:");
+
+// verificacion de que sea numeros
+if (!isNaN(numero)) {
+  // Convierte el numero a float 
+  numero = parseFloat(numero);
+
+  // verifica si el numero es positivo o negativo
+  if (numero > 0) {
+    console.log("El número ingresado es positivo.");
+  } else if (numero < 0) {
+    console.log("El número ingresado es negativo.");
+  } else {
+    console.log("El número ingresado es cero (positivo)");
+  }
+} else {
+  console.log("El valor ingresado no es un número.");
+}
+// ?quinta parte-end
+
+
+//?sexta parte
+// pide al usuario el numero de filas de la piramide
+let numeroFilas = prompt("Ingresa el número de filas de la pirámide:");
+
+// Verifica si el valor ingresado es un numero valido
+if (isNaN(numeroFilas)) {
+  console.log("El valor ingresado no es valido.");
+} else {
+  // Convertir el valor ingresado a un número entero
+  numeroFilas = parseInt(numeroFilas);
+
+  // Imprimir la pirámide
+  for (let i = numeroFilas; i >= 1; i--) {
+    let fila = "";
+    for (let j = 1; j <= i; j++) {
+      fila += "* ";
+    }
+    console.log(fila);
+  }
+}
+//?sexta parte-end
+
+
 
 
 
@@ -64,7 +166,7 @@ for (let i = 0; i < texto.length; i++) {
 
 // resultado 
 console.log("El número de letras en el texto es: " + contador);
-// ? ejercicio de samuel-end
+ // ? ejercicio de samuel-end
 
 
 
@@ -99,31 +201,3 @@ if (isNaN(base) || isNaN(exponente)) {
   console.log("El resultado de la potencia es: " + resultado);
 }
 //?correccion del ejercicio anterior  
-
-
-
-
-
-// ?quinta parte
-// solicitud de que el usuario ingrese un numero
-let numero = prompt("Ingresa un número:");
-
-// verificacion de que sea numeros
-if (!isNaN(numero)) {
-  // Convierte el numero a float 
-  numero = parseFloat(numero);
-
-  // verifica si el numero es positivo o negativo
-  if (numero > 0) {
-    console.log("El número ingresado es positivo.");
-  } else if (numero < 0) {
-    console.log("El número ingresado es negativo.");
-  } else {
-    console.log("El número ingresado es cero (positivo)");
-  }
-} else {
-  console.log("El valor ingresado no es un número.");
-}
-// ?quinta parte-end
-
-
