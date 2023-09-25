c950e5d21600d5d14ea8a720eeb5ab6e21fceb80
jose-alviarez <jose>
2023-09-25 14:25:33 -0400

    ultimos ejercicios

--- asset/js/4-cuarto-lote/5-fizzBuzz-string.js
+++ asset/js/4-cuarto-lote/5-fizzBuzz-string.js
@@ -1,13 +1,11 @@
 //? funcion que recibe un numero y retorna un string segun:
 //?"fizz" es multiplo de "3", "buzz" multiplo de "5", 
 //?"fizzbuzz" si es multiplo de los dos  
-
-
 function fizzBuzz() {
-    let numero = prompt("Ingresa un número:");
+    let numero = prompt("ingresa un numero:");
   
     if (isNaN(numero)) {
-      return "Error: Ingresa un valor numérico sin letras.";
+      return "ingresa un valor numerico";
     }
   
     let resultado = "";
@@ -16,7 +14,7 @@ function fizzBuzz() {
       case numero % 3 === 0 && numero % 5 === 0:
         resultado = "fizzbuzz";
         break;
-      case numero % 3 === 0:
+      case numero % 3 === 0: 
         resultado = "fizz";
         break;
       case numero % 5 === 0:
@@ -25,16 +23,10 @@ function fizzBuzz() {
       default:
         resultado = numero.toString();
         break;
-    }
-  
+    }  
     return resultado;
   }
-  
+
   console.log(fizzBuzz());
-  
-
-
-
-
 //?-end
 
